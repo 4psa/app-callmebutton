4ae413df929c21daabf6b1049e18627d9caedd74
Automation <bogdanc@intern.pch>
2015-05-18 18:48:37 +0300

    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 4PSA VoipNow App: CallMeButton
+ *
+ * This file stores all the configuration parameters like:
+ * IP/Hostname of the server
+ * OAuth credentials
+ *
+ * @version 2.0.0
+ * @license released under GNU General Public License
+ * @copyright (c) 2012 4PSA. (www.4psa.com). All rights reserved.
+ * @link http://wiki.4psa.com
+ */
+
+/**
+ * The IP/Hostname of the VoipNow Professional server
+ * @global string
+ */
+$config['VN_SERVER_IP'] = 'CHANGEME';
+
+/**
+ * Number of the extension that will initiate the call
+ * @global string
+ */
+$config['VN_EXTENSION'] =  'CHANGEME';
+
+/**
+ * APP ID for 3-legged OAuth
+ * Must be fetched from VoipNow interface
+ * @global string
+ */
+$config['OAUTH_APP_ID'] = 'CHANGEME';
+
+/**
+ * APP Secret for 3-legged OAuth
+ * Must be fetched from VoipNow interface
+ * @global string
+ */
+$config['OAUTH_APP_SECRET'] = 'CHANGEME';
+
+?>
